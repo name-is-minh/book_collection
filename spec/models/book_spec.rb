f1f6b3e6a0be9201dae00f5bbb8fd2a05f3a1e62
Minh <mhnguyen@tamu.edu>
2026-09-14 17:05:06 -0500

    fix request tests to verify corresponding flash notices

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Book, type: :model do
-  it "saves a book and shows the correct flash notice" do
+  it "saves a book with a title" do
     book = Book.new(title: "test67")
     expect(book.save).to be true
   end
 
-  it "doesnt save a book that has a blank title and shows the correct flash notice" do
+  it "doesnt save a book that has a blank title" do
     book = Book.new(title: "")
     expect(book.save).to be false
   end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe "Books", type: :request do
 
     expect(Book.last.title).to eq("test67")
     expect(response).to redirect_to(books_path)
+
+    follow_redirect!
+
+    expect(response.body).to include("Book was successfully created.")
   end
 
   it "doesnt save a book that has a blank title and shows the correct flash notice" do
@@ -14,5 +18,7 @@ RSpec.describe "Books", type: :request do
     post books_path, params: { book: { title: "" } }
 
     expect(Book.count).to eq(count)
+    expect(response).to have_http_status(:unprocessable_content)
+    expect(response.body).to include("Title can&#39;t be blank")
   end
 end
