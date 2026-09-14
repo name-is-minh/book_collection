929feb3837715795d14378c63e6ca6f0acab08ba
Minh <mhnguyen@tamu.edu>
2026-09-14 17:24:43 -0500

    add author attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_09_14_103430) do
+ActiveRecord::Schema[8.0].define(version: 2026_09_14_221725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_09_14_103430) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
   create_table "categories", force: :cascade do |t|
