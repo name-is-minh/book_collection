cef9ec08e00629fa5e8eb3b28bc569f2edee895a
Minh <mhnguyen@tamu.edu>
2026-09-14 17:13:38 -0500

    add tests for new book attributes

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -10,4 +10,26 @@ RSpec.describe Book, type: :model do
     book = Book.new(title: "")
     expect(book.save).to be false
   end
+
+  it "saves a book with an author" do
+    book = Book.new(title: "frieren", author: "Yamada")
+
+    expect(book.save).to be true
+    expect(book.author).to eq("Yamada")
+  end
+
+  it "saves a book with a price" do
+    book = Book.new(title: "frieren", price: 0.99)
+
+    expect(book.save).to be true
+    expect(book.price.to_f).to eq(0.99)
+  end
+
+  it "saves a book with a published date" do
+    date = Date.new(1000, 1, 1)
+    book = Book.new(title: "frieren", published_date: date)
+
+    expect(book.save).to be true
+    expect(book.published_date).to eq(date)
+  end
 end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -21,4 +21,40 @@ RSpec.describe "Books", type: :request do
     expect(response).to have_http_status(:unprocessable_content)
     expect(response.body).to include("Title can&#39;t be blank")
   end
+
+  it "saves a book with an author" do
+    post books_path, params: {
+      book: {
+        title: "frieren",
+        author: "Yamada"
+      }
+    }
+
+    expect(response).to redirect_to(books_path)
+    expect(Book.last.author).to eq("Yamada")
+  end
+
+  it "saves a book with a price" do
+    post books_path, params: {
+      book: {
+        title: "frieren",
+        price: 0.99
+      }
+    }
+
+    expect(response).to redirect_to(books_path)
+    expect(Book.last.price.to_f).to eq(12.99)
+  end
+
+  it "saves a book with a published date" do
+    post books_path, params: {
+      book: {
+        title: "frieren",
+        published_date: "1000-01-01"
+      }
+    }
+
+    expect(response).to redirect_to(books_path)
+    expect(Book.last.published_date).to eq(Date.new(1000, 1, 1))
+  end
 end
