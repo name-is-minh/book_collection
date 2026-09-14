2af6d18063ad7e3cd4d95f0f6e753b36f25f7e43
Minh <mhnguyen@tamu.edu>
2026-09-14 16:54:27 -0500

    handle book with blank title and add corresponding flash notices

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Book, type: :model do
-  it "saves a book" do
+  it "saves a book and shows the correct flash notice" do
     book = Book.new(title: "test67")
     expect(book.save).to be true
   end
 
-  it "doesnt save a book that has a blank title" do
+  it "doesnt save a book that has a blank title and shows the correct flash notice" do
     book = Book.new(title: "")
     expect(book.save).to be false
   end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "Books", type: :request do
-  it "creates a book with a title" do
+  it "saves a book and shows the correct flash notice" do
     post books_path, params: { book: { title: "test67" } }
 
     expect(Book.last.title).to eq("test67")
     expect(response).to redirect_to(books_path)
   end
 
-  it "doesnt save a book that has a blank title" do
+  it "doesnt save a book that has a blank title and shows the correct flash notice" do
     count = Book.count
 
     post books_path, params: { book: { title: "" } }
