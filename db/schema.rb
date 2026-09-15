fa53e7eb1fb690195770a01b5454f6674d547ef3
Minh <mhnguyen@tamu.edu>
2026-09-14 21:43:51 -0500

    add user scaffold with username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_09_14_222506) do
+ActiveRecord::Schema[8.0].define(version: 2026_09_15_024106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -37,5 +37,11 @@ ActiveRecord::Schema[8.0].define(version: 2026_09_14_222506) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
