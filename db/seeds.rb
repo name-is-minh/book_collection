2534ceea6bc84188ab7b11d4042872a76b5087ba
Minh <mhnguyen@tamu.edu>
2026-09-14 20:48:38 -0500

    add seed data and update procfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+books = [
+  {
+    title: "aot",
+    author: "jp",
+    price: 1.99,
+    published_date: Date.new(2000, 2, 2)
+  },
+  {
+    title: "csm",
+    author: "peak",
+    price: 1.99,
+    published_date: Date.new(1230, 1, 1)
+  },
+  {
+    title: "t1",
+    author: "kr",
+    price: 2.99,
+    published_date: Date.new(2014, 10, 9)
+  },
+  {
+    title: "goat",
+    author: "messi",
+    price: 10.00,
+    published_date: Date.new(1945, 6, 24)
+  },
+  {
+    title: "sleep",
+    author: "me",
+    price: 0.99,
+    published_date: Date.new(2026, 9, 15)
+  }
+]
+
+books.each do |attributes|
+  book = Book.find_or_initialize_by(title: attributes[:title])
+  book.update!(attributes)
+end
